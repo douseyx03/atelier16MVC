bf30981e174949874bde44d3fe0d8e29cf760b11
douseyx03 <dousey2003@gmail.com>
2023-10-31 16:49:40 +0000

    creation de la vue du MVC avec les des differents interfaces indispensables.

---
+++ vue/interface.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Ajout de contact</title>
+</head>
+<body>
+    <header>
+        <h1>Gestionnaire de contact</h1>
+    </header>
+    <main>
+        <form action="" method="post">
+            <label for="prenom">Prénom</label><br>
+            <input type="text" id="prenom" name="prenom" autocomplete="off" required><br>
+            <label for="nom">Nom</label><br>
+            <input type="text" id="nom" name="nom" autocomplete="off" required><br>
+            <label for="telephone">Numéro téléphone</label><br>
+            <input type="tel" id="telephone" name="telephone" required><br>
+            <label for="favori">Favori</label>
+            <select name="favori" id="favori">
+                <option value="0">Non</option> 
+                <option value="1">Oui</option>
+            </select><br>
+            <input type="submit" name="enregistrer" placeholder=" " value="Enregistrer">
+        </form><br>
+        <button type="button"><a href="liste.html" <!--target="_blank"-- rel="Liste des contacts">Voir la liste des contacts</a></button>
+        <!--<a href="liste.html"><input type="button" value="Voir la liste des contactes" ></a>-->
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
---
+++ vue/liste.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Liste des contacts</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Contacts </h1>
+    </header>
+    <main>
+        <div>
+            <h2>echo $prenom." ".$nom</h2>
+            <p>
+                echo $Numéro
+                echo $favori
+            </p>
+        </div>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
